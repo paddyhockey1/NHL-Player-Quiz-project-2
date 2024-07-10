2b6dad2fc88f1233abd7640e370fdbccdd3af873
Patrick Galvin <patjoegalvin@gmail.com>
2024-07-10 13:27:52 +0000

    Add: function for changing score when user answers question and changes whether answer is correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -357,15 +357,14 @@ const mdQuizQuestions = [
         }
         updateScores();
     }
-  
-  
+
     function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-    element.classList.add("correct");
-    } else {
-    element.classList.add("wrong");
-    }
+        clearStatusClass(element);
+        if (correct) {
+            element.classList.add("correct");
+        } else {
+            element.classList.add("wrong");
+        }
     }
   
     function clearStatusClass(element) {
